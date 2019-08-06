69f49c46dbf2dbf791ea11869ff389e864a254a6
rdmaloney <45108790+rdmaloney@users.noreply.github.com>
2019-08-06 19:53:59 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,77 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import string
+import re
+import datetime
+import sqlite3
+import time
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+e_name = []
+f1 = []
+f2 = []
+
+
+
+
+def scrape_data():
+
+        data = requests.get("http://ufcstats.com/statistics/events/upcoming")
+        soup = BeautifulSoup(data.text, 'html.parser')
+        table = soup.find('table', {"class": "b-statistics__table-events"})
+        links = table.find_all('a', href=True)
+
+        for link in links:
+            all_links.append(link.get('href'))
+
+        for link in all_links:
+            print(f"Now currently scraping link: {link}")
+
+            data = requests.get(link)
+            soup = BeautifulSoup(data.text, 'html.parser')
+            time.sleep(1)
+
+            h2 = soup.find("h2")
+            e_name.append(h2.text.strip())
+
+            rows = soup.find_all('table', {"class": "b-fight-details__table b-fight-details__table_style_margin-top b-fight-details__table_type_event-details js-fight-table"})
+
+            for row in rows:
+
+
+                    fighters = row.find_all('a', {"href": re.compile("http://ufcstats.com/fighter-details")})
+
+                    try:
+                        f1.append(fighters[0].text.strip())
+                        f2.append(fighters[1].text.strip())
+                    except IndexError:
+                        f1.append("null")
+                        f2.append("null")
+                    continue
+
+
+        return None
+
+#preprocessing
+# remove rows where DOB is null
+# impute stance as orthodox for missing stances
+def create_df():
+    #create empty dataframe
+    df = pd.DataFrame()
+
+    df["Event"] = e_name
+    df["Fighter1"] = f1
+    df["Fighter2"] = f2
+
+    return df
+
+scrape_data()
+df = create_df()
+print("Scraping completed")
+
+conn = sqlite3.connect('data.sqlite')
+df.to_sql('data', conn, if_exists='replace')
+print('Db successfully constructed and saved')
+conn.close()
