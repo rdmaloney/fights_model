6894f3c12b99a66184673bbffef69367c401b266
rdmaloney <45108790+rdmaloney@users.noreply.github.com>
2019-08-12 00:16:12 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -32,7 +32,7 @@ def scrape_data():
         h2 = soup.find("h2")
         e_name.append(h2.text.strip())
 
-        fights_table = soup.find("table").find("tbody").find_all("tr")
+        fights_table = soup.find('table').find('tbody').find('tr').find_next('tr')
 
         fighters = fights_table.find_all('a', {"href": re.compile("http://ufcstats.com/fighter-details")})
 
